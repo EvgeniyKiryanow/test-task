87d7a9756f326909bf0a693f5e8fc32389fd9d53
Evgeniy Kiryanow <kirjeka19977@gmail.com>
2024-06-19 20:12:10 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,16 +3,6 @@
 // Start npm run dev -- -o
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  head: {
-    titleTemplate: '%s - My Test Nuxt app', 
-    htmlAttrs: {
-      lang: 'en', 
-    },
-    meta: [
-      { charset: 'utf-8' }, 
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }, 
-    ],
-  },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
   css: ["@/assets/fonts/fonts.css", "@/assets/css/variables.css"],
   build: {
